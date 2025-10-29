91af9f96ffc1b9a9afaac69640e9063ee35e916b
Aly <gitaly2433110@gmail.com>
2025-10-29 14:13:20 +0800

    Student.java

--- Student.java
+++ Student.java
@@ -1,98 +1,103 @@
-package Method;
-
+package Methods;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-public class Student extends Book{
+public class Student extends Book {
     private Scanner sc = new Scanner(System.in);
-    public HashMap<Integer, String> borrowed = new HashMap<>();
-    public HashMap<Integer, String> returned = new HashMap<>();
-    public ArrayList<User> user = new ArrayList<>();
-    public void login(){
-        System.out.println(" - ".repeat(7) + "Welcome Scholar of Batangas State University Lipa Campus!" + " - ".repeat(7));
-        System.out.print("Enter G-Suite: ");
+    ArrayList<User> user = new ArrayList<>();
+    HashMap<Integer, String> borrow = new HashMap<>();
+    HashMap<Integer, String> return_book = new HashMap<>();
+
+    Welcome welcome = new Welcome() {
+        @Override
+        void welcome() {
+            System.out.println(" - ".repeat(7) + " Welcome Student! " + " - ".repeat(7));
+        }
+    };
+
+    public void getInfo() {
+        System.out.println(" - ".repeat(7) + " Student Login " + (" - ".repeat(7)));
+        System.out.print("\tEnter G-Suite: ");
         String g_suite = sc.nextLine();
-        System.out.print("Enter Password: ");
+        System.out.print("\tSr-Code: ");
         String sr_code = sc.nextLine();
-        if (sr_code.matches("\\d{2}-\\d{5}")){
-            LocalDate date_login = LocalDate.now();
-            LocalTime time_login = LocalTime.now();
-            User newLogin = new User(g_suite, sr_code, date_login, time_login);
+        if (sr_code.matches("\\d{2}-\\d{5}")) {
+            LocalTime localTime = LocalTime.now();
+            LocalDate localDate = LocalDate.now();
+            User newLogin = new User(g_suite, sr_code, localTime, localDate);
             user.add(newLogin);
-            System.out.println("\f");
-            System.out.println(" - ".repeat(7) + "Available Books" + " - ".repeat(7));
-            availableBook();
-            showBook();
-            System.out.println(" - ".repeat(20));
-            while (true){
+            welcome.welcome();
+            System.out.println();
+            while (true) {
                 System.out.println("[1] Borrow Book\n[2] Return Book\n[3] Exit");
-                System.out.print("Enter a number: ");
-                int student_choice = sc.nextInt();
-                if (student_choice == 1){
+                System.out.print("Enter number: ");
+                int enter = sc.nextInt();
+                if (enter == 1) {
                     borrowBook();
-                }else if(student_choice == 2){
+                } else if (enter == 2) {
                     returnBook();
-                }else if(student_choice == 3){
+                } else if (enter == 3) {
                     break;
-                }else{
-                    System.out.println("Invalid input!");
+                } else {
+                    System.out.println("Invalid input. Please try again.");
                 }
             }
-        }else{
-            System.out.println("Invalid credentials.");
         }
     }
 
-    public void showBook(){
-        for (Map.Entry<Integer, String> value : book.entrySet()){
-            Integer id = value.getKey();
-            String title = value.getValue();
-            System.out.println("Book ID: " + id + " Title: " + title);
-        }
-    }
-    public void borrowBook(){
-        System.out.print("Book ID you want to borrow: ");
+    public void borrowBook() {
+        Welcome borrowedWelcome = new Welcome() {
+            @Override
+            void welcome() {
+            }
+        };
+        borrowedWelcome.welcome();
+        availableBooks();
+        showBooks();
+        System.out.print("\t\tBook id: ");
         int book_id = sc.nextInt();
         if (book.containsKey(book_id)){
             String title = book.get(book_id);
-            borrowed.put(book_id, title);
-            System.out.println("Successfuly Borrowed.");
+            borrow.put(book_id, title);
+            System.out.println(" - ".repeat(22));
+            System.out.println("\t\t Successfully borrowed title: " + title);
+            System.out.println(" - ".repeat(22));
         }else{
-            System.out.println("Invalid ID.");
+            System.out.println("No book id like that.");
         }
     }
 
     public void returnBook(){
-        if (borrowed.isEmpty()){
-            System.out.println("No borrowed book yet.");
-            return;
-        }
-        System.out.println(" - ".repeat(5) + "Book Borrowed" + " - ".repeat(5));
-        for (Map.Entry<Integer, String> value2 : borrowed.entrySet()){
-            int id = value2.getKey();
-            String title = value2.getValue();
-            System.out.println("ID: " + id + " Title: " + title);
-        }
-        System.out.print("Enter book id you want to return: ");
-        int return_book = sc.nextInt();
-        if (borrowed.containsKey(return_book)){
-            String returnedTitle = borrowed.get(return_book);
-            borrowed.remove(return_book);
-            returned.put(return_book, returnedTitle);
+        Welcome returnWelcome = new Welcome() {
+            @Override
+            void welcome() {
+                System.out.println(" - ".repeat(7) + " Return Book " + " - ".repeat(7));
+            }
+        };
+        if (borrow.isEmpty()){
+            System.out.println("No book to return yet.");
         }else{
-            System.out.println("Invalid book id.");
+            returnWelcome.welcome();
+            for (Map.Entry<Integer, String> returnbook : borrow.entrySet()){
+                Integer id = returnbook.getKey();
+                String title = returnbook.getValue();
+                System.out.println("\tID: " + id + " Title: " + title);
+            }
+            System.out.print("\t\tReturn Book id: ");
+            int bookID = sc.nextInt();
+            if (borrow.containsKey(bookID)){
+                String title = borrow.get(bookID);
+                return_book.put(bookID, title);
+                borrow.remove(bookID, title);
+                System.out.println(" - ".repeat(22));
+                System.out.println("\t\t Successfully return the book: " + title);
+                System.out.println(" - ".repeat(22));
+            }
         }
-
     }
-    public void showUser(){
-        for (User u : user){
-            System.out.println("G-Suite: " + u.getGsuite());
-            System.out.println("Sr-Code: " + u.getSr_code());
-            System.out.println("Date Login: " + u.getDate_login());
-            System.out.println("Time Login: " + u.getTime_login());
-        }
-    }
-
 }
