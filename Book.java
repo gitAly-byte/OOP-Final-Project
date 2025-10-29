2ce8070d0ebe4e812e6f620d6e7fb4c3a8b64ba2
Aly <gitaly2433110@gmail.com>
2025-10-29 14:13:41 +0800

    Book.java

--- Book.java
+++ Book.java
@@ -1,17 +1,30 @@
-package Method;
+package Methods;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Book {
-    public HashMap<Integer, String> book = new HashMap<>();
-    void availableBook(){
+    HashMap<Integer, String> book = new HashMap<>();
+    Welcome welcome = new Welcome() {
+        @Override
+        void welcome() {
+            System.out.println(" - ".repeat(7) + " Welcome to Library " + " - ".repeat(7));
+        }
+    };
+    public void availableBooks() {
+        welcome.welcome();
         book.put(1, "Computer Programming");
         book.put(2, "Advance Computer Programming");
-        book.put(3, "Database Managament");
-        book.put(4, "Objected-Oriented-Programming");
-        book.put(5, "Networking 1");
+        book.put(3, "Database Management");
+        book.put(4, "Networking 1");
+        book.put(5, "Physics-Calculus");
         book.put(6, "Discrete Mathematics");
-        book.put(7, "Physic-Calculus");
+    }
+    public void showBooks(){
+        for (Map.Entry<Integer, String> entry : book.entrySet()){
+            Integer id = entry.getKey();
+            String title = entry.getValue();
+            System.out.println("\tID: " + id + " Title: " + title);
+        }
     }
 }
